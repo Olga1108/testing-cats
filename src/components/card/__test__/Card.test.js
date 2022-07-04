ebd25919e0fea6bb1ad912b05ab257651c769202
Olha Pushkar <Olha_Pushkar@epam.com>
2022-07-04 18:24:16 +0300

    add: card

--- src/components/card/__test__/Card.test.js
+++ src/components/card/__test__/Card.test.js
@@ -2,22 +2,37 @@ import { toBeInTheDocument } from '@testing-library/jest-dom/dist/matchers';
 import {render, screen} from '@testing-library/react';
 import Card from '../Card';
 
+const cardProps = {
+    name: 'Sydney',
+    phone: '111-111-111',
+    email: 'olha@gmail.com',
+    image: {
+        url: 'https://images.unsplash.com/photo-1573865526739-10659fec78a5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2F0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60', 
+        alt: 'cute cat',
+    },
+    favoured: false,
+}
+
 describe('Card', () => {
     test('should show name of cat', () => {
         render(
             <Card 
-                name='Sydney'
-                phone='111-111-111'
-                email='olha@gmail.com'
-                image={{
-                    url: 'https://images.unsplash.com/photo-1573865526739-10659fec78a5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2F0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60', 
-                    alt: 'cute cat'
-                }}
-                favoured={false}
+               {...cardProps} 
             />
         );
         expect(screen.getByRole('heading', {
             name: /sydney/i
         })).toBeInTheDocument()
+    });
+
+    test('should show phone number of cat', () => {
+        render(
+            <Card 
+               {...cardProps} 
+            />
+        );
+        expect(
+            screen.getByText(/111-111-111/i)
+        ).toBeInTheDocument()
     })
 })
